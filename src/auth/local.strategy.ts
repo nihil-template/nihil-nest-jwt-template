320f6933a3cf03838db4011a64a8a77da98306ff
NIHILncunia <nihil_ncunia@naver.com>
2022-03-04 10:40:30 +0900

    2022 0304 응답코드 수정

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -1,6 +1,6 @@
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
-import { HttpException, Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserWithOutPassword } from '@/types/user.types';
 
@@ -22,7 +22,7 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
     if (!user) {
       throw new HttpException({
         message: '아이디 혹은 비밀번호가 일치하지 않습니다.',
-      }, 401);
+      }, HttpStatus.UNAUTHORIZED);
     }
 
     return user;
